86387f809113f3184bf1e6ae504d869b7066ecfa
ben <phas0ruk@gmail.com>
2022-08-31 22:18:09 -0700

    completes basic functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.post_detail_get = (req, res) => {
     Post.findById(req.params.id).populate("author").exec((err, post) => {
@@ -10,3 +11,23 @@ exports.post_detail_get = (req, res) => {
         }
     });
 }
+
+exports.post_delete = (req, res) => {
+    // check sender is logged in and an admin
+    if(!req.user) { // log in check
+        res.render("post", { user: req.user, error: "You must be logged in to delete a post" });
+    } else if(!req.user.admin){ // admin check
+        res.render("post", { user: req.user, error: "You must be an admin to delete a post" });
+    } else {
+        Post.findByIdAndDelete(req.params.id, (err, post) => {
+            if (err) {
+                console.log(err);
+                res.render("post", { user: req.user, error: "Error deleting post" });
+            } else {
+                res.redirect("/");
+            }
+        });
+    }
+}
+
+
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+router.get('/delete-post/:id', postController.post_delete);
 router.get('/:id', postController.post_detail_get);
 
 module.exports = router;
